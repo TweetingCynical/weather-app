e69a9e3b16a6a163cba7955cb762692f26f3953d
Al Turing <jongs@Jons-iMac.local>
2023-02-05 14:35:41 +0000

    Added styling. Refactored CSS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,7 +69,7 @@ function getWeather(location) {
   // Extract location data from coordinates api
   let latitude = location.lat;
   let longitude = location.lon;
-  let cityName = location.name;
+  let cityName = `${location.name}, ${location.country}`;
 
   // Use coordinates to get weather data
   let queryWeatherURL = `${initAPIURL}/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${myKey}`;
@@ -84,17 +84,20 @@ function getWeather(location) {
   });
 }
 
+// Creates the TODAY section filled with current weather data from api
 function showCurrentWeather(city, weatherInfo) {
   // Set date
-  const displayDate = moment().format("dddd, Do MMMM YYYY");
-  const icon = `https://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`;
-  // const type = weatherInfo.weather[0].main;
+  const displayDate = moment().format("ddd, Do MMM YYYY");
+
+  // Store values from api fetch
+  const icon = `${initAPIURL}/img/w/${weatherInfo.weather[0].icon}.png`;
   const description = weatherInfo.weather[0].description;
   const tempC = weatherInfo.main.temp.toFixed(0);
   const wind = (weatherInfo.wind.speed / 1.61).toFixed(1);
   const humidity = weatherInfo.main.humidity;
 
-  const card = $("<div>").addClass("card shadow p-3 mb-5 bg-white rounded");
+  // Create elements for section
+  const card = $("<div>").addClass("card shadow-lg p-3 mb-5 bg-white rounded");
   const cardBody = $("<div>").addClass("card-body");
   const titleDiv = $("<div>").addClass("title-div");
   const cardTitle = $("<h3>")
@@ -105,16 +108,15 @@ function showCurrentWeather(city, weatherInfo) {
     .addClass("icon")
     .attr("src", icon)
     .attr("alt", description);
-  iconBG.append(iconEl);
-  titleDiv.append(cardTitle, iconBG);
-  // const typeEl = $("<p>")
-  //   .addClass("card-text")
-  //   .text(`${type} (${description})`);
   const tempEl = $("<p>").addClass("card-text").text(`Temperature: ${tempC}°C`);
   const windEl = $("<p>").addClass("card-text").text(`Wind: ${wind} mph`);
   const humidityEl = $("<p>")
     .addClass("card-text")
     .text(`Humidity: ${humidity}%`);
+
+  // Append elements to section
+  iconBG.append(iconEl);
+  titleDiv.append(cardTitle, iconBG);
   cardBody.append(titleDiv, tempEl, windEl, humidityEl);
   card.append(cardBody);
 
@@ -126,10 +128,61 @@ function showCurrentWeather(city, weatherInfo) {
 
 // Create elements to display forecasted weather
 function showForecastWeather(weatherInfo) {
+  forecast.html("");
   // Filter out the records from weatherInfo which are not midday forecasts
   let dailyForecast = weatherInfo.filter(function (info) {
     return info.dt_txt.includes("12:00:00");
   });
+
+  // Create overall forecast area card elements
+  const cardContainer = $("<div>").addClass("card shadow-lg mb-3 rounded");
+  const cardBodyContainer = $("<div>").addClass(
+    "card-body text-white forecast-cards"
+  );
+  const cardBodyTitle = $("<h3>")
+    .addClass("h3 text-center mt-3")
+    .text(`5-day Forecast`);
+  cardContainer.append(cardBodyTitle, cardBodyContainer);
+  forecast.append(cardContainer);
+
+  // For loop to create the cards for each of the next 5 days
+  for (let i = 0; i < dailyForecast.length; i++) {
+    // Store values from api fetch
+    const displayDate = moment(
+      dailyForecast[i].dt_txt,
+      "YYYY-MM-DD HH:mm:ss"
+    ).format("ddd");
+    const icon = `${initAPIURL}/img/w/${dailyForecast[i].weather[0].icon}.png`;
+    const description = dailyForecast[i].weather[0].description;
+    const tempC = dailyForecast[i].main.temp.toFixed(0);
+    const wind = (dailyForecast[i].wind.speed / 1.61).toFixed(1);
+    const humidity = dailyForecast[i].main.humidity;
+
+    // Create elements for card container
+    const card = $("<div>").addClass(
+      "card-body shadow-lg mx-1 mb-2 rounded col-md forecast-card"
+    );
+    const cardTitle = $("<h5>")
+      .addClass("h5 text-white")
+      .text(`${displayDate}`);
+    const iconBG = $("<div>").addClass("icon-bg-sm");
+    const iconEl = $("<img>")
+      .addClass("icon-sm")
+      .attr("src", icon)
+      .attr("alt", description);
+    iconBG.append(iconEl);
+    const tempEl = $("<p>").addClass("card-text").text(`Temp: ${tempC}°C`);
+    const windEl = $("<p>").addClass("card-text").text(`Wind: ${wind}mph`);
+    const humidityEl = $("<p>")
+      .addClass("card-text")
+      .text(`Humidity: ${humidity}%`);
+
+    // Append all card elements to the card
+    card.append(cardTitle, iconBG, tempEl, windEl, humidityEl);
+
+    // Append card to its container
+    cardBodyContainer.append(card);
+  }
 }
 
 // Search form submit function
